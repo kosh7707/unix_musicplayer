720b45204f0b6e3bafcbe294ce5446f302f0493a
chagieun <chagieun@gmail.com>
2022-05-18 10:11:46 +0900

    0518

--- create_music_titles.c
+++ create_music_titles.c
@@ -1,5 +1,16 @@
-#include <stdio.h>
+#include "textfilewriter.h"
 
 int main(){
+	FILE* title;
+        title = fopen("title.txt", "w");
+
+        int num;
+        char musicTitle[51];
+        scanf("%d", &num);
+        for(int i=0; i<=num; i++){
+                read_file(title);
+		write_file(title);
+        }
+        fclose(title);
 
 }
