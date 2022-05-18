b912855f546a317a49faced853f8d276e1a939ca
namhyung123 <namhyung123@pusan.ac.kr>
2022-05-18 21:32:04 +0900

    FEAT : Print current linked_list list
    Outputs a playlist to the console

--- libs/linkedlist.c
+++ libs/linkedlist.c
@@ -6,6 +6,17 @@ static Node* _head = NULL;
 static Node* _tail = NULL;
 static Node* _cur_node = NULL;
 
+void print(){
+    //재생목록을 콘솔에 출력함
+    Node* temp = _head;
+    printf("LinkedList [ ");
+    while(temp != NULL){
+        printf("%s ",temp->data);
+        temp = temp->next;
+    }
+    printf("]\n");
+};
+
 Node* append_left(size_t n, char new_data[n]){
     //재생 목록의 첫 번째 위치에 노래를 추가함
     Node* newNode;
