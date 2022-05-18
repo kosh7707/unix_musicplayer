515ff77e80cdf90c7d141cb213f96aaa83c469a8
namhyung123 <namhyung123@pusan.ac.kr>
2022-05-18 21:22:27 +0900

    FEAT : Insert node at first location
    Add a song to the first location in the playlist

---
+++ libs/linkedlist.c
@@ -0,0 +1,28 @@
+//linkedlist.c
+
+#include "linkedlist.h"
+
+static Node* _head = NULL;
+static Node* _tail = NULL;
+static Node* _cur_node = NULL;
+
+Node* append_left(size_t n, char new_data[n]){
+    //재생 목록의 첫 번째 위치에 노래를 추가함
+    Node* newNode;
+    newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = new_data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    //printf("%s\n", newNode->data);
+    if(empty()){
+        _head = newNode;
+        _tail = newNode;
+    }
+    else {
+        newNode->next = _head;
+        _head->prev = newNode;
+        _head = newNode;
+    }
+    //printf("%s\n", _head->data);
+    return newNode;
+};
